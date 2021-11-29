99469844139e6dd7c01367d8e61ae361195647a4
izad-dev <mohammad.saein@gmail.com>
2021-11-29 20:08:31 +0330

    create student route

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const experss = require('express')
+const studentroute = require('./src/students/routes');
 const app = experss();
 const port = 3000;
 
 app.get("/",(req,res) => {
     res.send('hello world!');
 });
+app.use('/api/v1/student',studentroute);
 app.listen(port,()=> console.log(`app listening to port ${port}`));
 
