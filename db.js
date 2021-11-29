ede19da978523c33af2402d70891586236e56524
محمد ایزدپناه <m.izadpanah@RZDC.net>
2021-11-29 16:04:44 +0330

    define student route

--- db.js
+++ db.js
@@ -3,8 +3,12 @@ const Pool = require("pg").pool;
 const pool = new Pool(
     {
 user: "postgres",
-password: ""
-
-
+host: "localhost",
+database: "person",
+password: "f32r94q9wc849yb",
+port: 5432,
     }
 );
+
+module.exports = pool;
+ 
