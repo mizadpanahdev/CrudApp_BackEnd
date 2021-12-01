b167f71ed75b36870f07ab870704f06dfa919305
محمد ایزدپناه <m.izadpanah@RZDC.net>
2021-12-01 16:19:22 +0330

    add new

--- src/students/routes.js
+++ src/students/routes.js
@@ -2,5 +2,6 @@ const {Router} = require('express');
 const  router = Router();
 const controller = require('./controller')
 router.get("/",controller.getStudents);
+router.post("/",controller.addStudents);
 router.get("/:id",controller.getStudentById)
 module.exports = router;
