7325f229badbb604bf35adb4159f6102a0ff5c51
mohammad <m.izadpanah@razi24.ir>
2021-12-07 16:01:38 +0330

    section 2 of addsurdent

--- src/students/queries.js
+++ src/students/queries.js
@@ -1,8 +1,10 @@
 const getStudents = " SELECT * FROM person;"
 const getStudentsById = "SELECT * FROM person WHERE id= $1;"
 const checkStudentByEmail = "SELECT s FROM person s WHERE s.email= $1;"
+const addStudentToDb = "INSERT INTO person (first_name,last_name,email,gender,date_of_birth,country_of_birth) VALUES ($1,$2,$3,$4,$5,$6);"
 module.exports= {
     getStudents,
     getStudentsById,
     checkStudentByEmail,
+    addStudentToDb,
 }
--- src/students/routes.js
+++ src/students/routes.js
@@ -2,6 +2,6 @@ const {Router} = require('express');
 const  router = Router();
 const controller = require('./controller')
 router.get("/",controller.getStudents);
-router.post("/",controller.checkStudentByEmail);
+router.post("/",controller.addStudent);
 router.get("/:id",controller.getStudentById);
 module.exports = router;
