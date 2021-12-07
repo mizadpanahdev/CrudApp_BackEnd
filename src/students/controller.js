edf07969efa3b0d1cbfd1ac8d7e02ec8e37dd730
mohammad <m.izadpanah@razi24.ir>
2021-12-07 13:05:03 +0330

    update

--- src/students/controller.js
+++ src/students/controller.js
@@ -7,14 +7,24 @@ const getStudents = (req,res)=> {
         res.status(200).json(result.rows);
     })
 }
+const checkStudentByEmail =(req,res)=>{
+    const {id,first_name,last_name,email,gender,date_of_birth,country_of_birth}= req.body;
+    pool.query(queris.checkStudentByEmail,[email],(error,result)=>{
+        if (result.rows.length){
+            res.send('the email is exist');
+        } 
+    })
+
+};
 const getStudentById= (req,res)=>{
     const id = parseInt(req.params.id);
     pool.query(queris.getStudentsById,[id],(error,result)=>{
         if (error) throw error;
         res.status(200).json(result.rows);
     })
-}
+};
 module.exports = {
     getStudents,
   getStudentById,  
+  checkStudentByEmail,
 };
--- src/students/queries.js
+++ src/students/queries.js
@@ -1,6 +1,8 @@
 const getStudents = " SELECT * FROM person;"
 const getStudentsById = "SELECT * FROM person WHERE id= $1;"
+const checkStudentByEmail = "SELECT s FROM person s WHERE s.email=$1;"
 module.exports= {
     getStudents,
     getStudentsById,
+    checkStudentByEmail,
 }
