c16161a71e481ec6871cf854aca706007f52df5b
محمد ایزدپناه <m.izadpanah@RZDC.net>
2021-12-22 16:17:53 +0330

    change

--- cruadapp/src/students/routes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const  router = Router();
-const controller = require('./controller')
-router.get("/",controller.getStudents);
-router.post("/",controller.addStudent);
-router.delete("/:id",controller.removestudent);
-router.get("/:id",controller.getStudentById);
-module.exports = router;
--- src/students/controller.js
+++ src/students/controller.js
@@ -1,4 +1,4 @@
-const e = require('express');
+
 const { query } = require('express');
 const pool = require('../../db');
 const queris =require('./queries');
@@ -15,7 +15,7 @@ const addStudent =(req,res)=>{
         if (result.rows.length){
             res.send("the email is exist");
         } 
-    // add student if the emial does'nt exist in db
+    // add student if the emial doesn't exist in db
     pool.query(queris.addStudentToDb,[first_name,last_name,email,gender,date_of_birth,country_of_birth],(error,result)=>{
         if (error) throw error;
         res.status('201').send("the student created successsfully!");
@@ -26,12 +26,28 @@ const addStudent =(req,res)=>{
 const getStudentById= (req,res)=>{
     const id = parseInt(req.params.id);
     pool.query(queris.getStudentsById,[id],(error,result)=>{
-        if (error) throw error;
+        if (!result.rows.length) {
+            res.send("the student dosen't exist in database")
+        }
+        else
         res.status(200).json(result.rows);
     })
 };
+// method remove
+const removestudent = (req,res)=>{
+    //check the id is exist in db? and send message if is not
+    const id = parseInt(req.params.id);
+    pool.query(queris.getStudentById,[id],(error,result)=>{
+        if(!result.rows.length) {
+            res.status(200).send("the student doesn't exist in database")
+        }
+    })
+    // remove the student
+    
+}
 module.exports = {
   getStudents,
   getStudentById,  
   addStudent,
+  removestudent
 };
